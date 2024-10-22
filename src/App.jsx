091a0a51aee8e3bf6d92deb5463a2881f0598db6
Duc Vuong <82869802+DVfromtheEast@users.noreply.github.com>
2024-10-22 11:36:41 +0700

    update page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { useState } from 'react';
 import Sdwebgis from './components/sdwebgis.jsx';
+import Map from './components/map.jsx';
 import './App.css';
 
 function App() {
@@ -8,7 +9,22 @@ function App() {
 
   return (
     <>
-      <Sdwebgis className="sdlogo" />
+      <div className="sdlogo">
+        <Sdwebgis />
+      </div>
+      <div className="map">
+        <div className="marker m1"></div>
+        <div className="marker m2"></div>
+        <Map />
+      </div>
+      <div className="credit">
+        <p className="link">
+          Developed by{' '}
+          <a href="https://spatialdecisions.com/" target="blank_">
+            Spatial Decisions
+          </a>
+        </p>
+      </div>
     </>
   );
 }
